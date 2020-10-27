a6eb8a2a90c9837e750b88b1bd0f0bcd93a133f4
marcosvella <62914841+marcosvella@users.noreply.github.com>
2020-10-26 23:55:48 -0300

    Desafio #2 Rocketseat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url , techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo)
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const { url, techs, title } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if(repoIndex < 0){
+    return response.status(400).json({ error: 'Project not found' });
+  }
+  repositories[repoIndex] = {
+    id,
+    url,
+    techs,
+    title,
+    likes: 0
+  }
+
+  return response.json(repositories[repoIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if(repoIndex < 0){
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if(repoIndex < 0){
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  repositories[repoIndex].likes++;
+
+  return response.json(repositories[repoIndex]);
+
 });
 
 module.exports = app;
